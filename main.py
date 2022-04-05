29c12418199b70cc92777035b0997d7fc586974c
roman <romant56@mail.ru>
2022-04-05 21:48:37 +0300

    Зарегистрировал бота Игоря: @Yandex_Igor_bot и сделал простой шаблон ответа на сообщение

--- main.py
+++ main.py
@@ -1,11 +1,53 @@
-from flask import Flask
+# Импортируем необходимые классы.
+import logging
+from telegram.ext import Updater, MessageHandler, Filters
 
-app = Flask(__name__)
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
 
-@app.route('/')
-def index():
-    return "Hello"
+logger = logging.getLogger(__name__)
+
+TOKEN = '5110951414:AAF17EXuVIoLbUcDzieFwTF-WqwGtfQD1dM'
 
 
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, сам бот и класс updater, принявший сообщение.
+def echo(update, context):
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
+    update.message.reply_text('Привет, это пробная попытка')
+
+
+def main():
+    # Создаём объект updater.
+    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    updater = Updater(TOKEN)
+
+    # Получаем из него диспетчер сообщений.
+    dp = updater.dispatcher
+
+    # Создаём обработчик сообщений типа Filters.text
+    # из описанной выше функции echo()
+    # После регистрации обработчика в диспетчере
+    # эта функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+    text_handler = MessageHandler(Filters.text, echo)
+
+    # Регистрируем обработчик в диспетчере.
+    dp.add_handler(text_handler)
+    # Запускаем цикл приема и обработки сообщений.
+    updater.start_polling()
+
+    # Ждём завершения приложения.
+    # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
+    updater.idle()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
-    app.run(debug=False)
+    main()
