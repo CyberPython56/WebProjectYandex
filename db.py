90b4f3371ffc7380815eb8d11df2a80af141a5d7
Ivan <vanya.kharchenko.2018@mail.ru>
2022-04-19 15:29:55 +0500

    Ваня

---
+++ db.py
@@ -0,0 +1,12 @@
+import sqlite3
+from flask import Flask, render_template, url_for, request, g, flash, abort
+from flask_sqlalchemy import SQLAlchemy
+from FDataBase import FDataBase
+import os
+
+
+DATABASE = '/tmp/flslite.db'
+DEBUG = True
+SECRET_KEY = 'dhis788dsoiah78'
+
+app = Flask(__name__)
--- web.py
+++ web.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, g, flash, abort
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from FDataBase import FDataBase
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///YandexProject.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=True)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
