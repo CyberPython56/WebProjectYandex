281c044de70497725993392424f809b2dabd7dad
Ivan <vanya.kharchenko.2018@mail.ru>
2022-04-21 21:48:23 +0500

    123

--- web.py
+++ web.py
@@ -1,46 +1,56 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-
-
+import sqlite3
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///YandexProject.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = False
-db = SQLAlchemy(app)
+sqlite_connection = sqlite3.connect('YandexProject.sqlite', check_same_thread=False)
+cursor = sqlite_connection.cursor()
 
+def valid(login: str, password: str):
+    req = f"""
+SELECT * FROM "Admins"
+"""
+    resp = cursor.execute(req).fetchall()
+    if len(resp) == 0:
+        return False
+    for i in resp:
+        if i[1] == login and i[2] == password:
+            return True
+    return False
 
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    intro = db.Column(db.String(300), nullable=True)
-    text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+def get_bookings():
+    req = f"""
+SELECT * FROM "Booking"
+"""
+    resp = cursor.execute(req).fetchall()
 
-    def __repr__(self):
-        return '<Article %r>' % self.id
+    data = []
+    for i in reversed(resp):
+        d = {
+            "id": i[0],
+            "computer_id": i[1],
+            "name": i[2],
+            "date": i[3],
+            "time_start": i[4],
+            "time_finish": i[5],
+            "full_price": i[6]
+        }
+        data.append(d)
 
+    return data
 
 @app.route('/')
-@app.route('/home')
-def index():
-    return render_template("index.html")
+def login_page():
+    return render_template("login.html")
 
-
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
-
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return "User page: " + name + " - " + str(id)
-
-
-@app.route('/create')
-def create():
-    return render_template("create.html")
-
+@app.route('/booking/')
+def booking_page():
+    username = request.args.get('login')
+    password = request.args.get('password')
+    if valid(username, password):
+        return render_template("bookings.html", orders=get_bookings())
+    else:
+        return "Invalid login or Password"
 
 if __name__ == "__main__":
     app.run(debug=True)
